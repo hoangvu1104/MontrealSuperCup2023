2df1df9878dcef20008f0ae3cb01f0dda8700aea
Hoang Vu Pham <hoangvu1104@gmail.com>
2023-06-17 06:44:01 -0400

    Update url image

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,45 +3,45 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '',    
-    redirectTo: 'player-result',
+    path: '',
+    redirectTo: 'result',
     pathMatch: 'full'
   },
   {
     path: 'home',
-    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
-  },  
+    loadChildren: () => import('./pages/home/home.module').then(m => m.HomePageModule)
+  },
   {
     path: 'result',
-    loadChildren: () => import('./pages/result/result.module').then( m => m.ResultPageModule)
+    loadChildren: () => import('./pages/result/result.module').then(m => m.ResultPageModule)
   },
   {
     path: 'begin',
-    loadChildren: () => import('./pages/begin/begin.module').then( m => m.BeginPageModule)
+    loadChildren: () => import('./pages/begin/begin.module').then(m => m.BeginPageModule)
   },
   {
     path: 'playoff',
-    loadChildren: () => import('./pages/playoff/playoff.module').then( m => m.PlayoffPageModule)
+    loadChildren: () => import('./pages/playoff/playoff.module').then(m => m.PlayoffPageModule)
   },
   {
     path: 'playoffnormal',
-    loadChildren: () => import('./pages/playoffnormal/playoffnormal.module').then( m => m.PlayoffnormalPageModule)
-  },  
+    loadChildren: () => import('./pages/playoffnormal/playoffnormal.module').then(m => m.PlayoffnormalPageModule)
+  },
   {
     path: 'team-info/:teamName',
-    loadChildren: () => import('./pages/team-info/team-info.module').then( m => m.TeamInfoPageModule)
+    loadChildren: () => import('./pages/team-info/team-info.module').then(m => m.TeamInfoPageModule)
   },
   {
     path: 'tables',
-    loadChildren: () => import('./pages/tables/tables.module').then( m => m.TablesPageModule)
+    loadChildren: () => import('./pages/tables/tables.module').then(m => m.TablesPageModule)
   },
   {
     path: 'table-result',
-    loadChildren: () => import('./pages/table-result/table-result.module').then( m => m.TableResultPageModule)
+    loadChildren: () => import('./pages/table-result/table-result.module').then(m => m.TableResultPageModule)
   },
   {
     path: 'player-result',
-    loadChildren: () => import('./pages/player-result/player-result.module').then( m => m.PlayerResultPageModule)
+    loadChildren: () => import('./pages/player-result/player-result.module').then(m => m.PlayerResultPageModule)
   }
 ];
 
@@ -52,4 +52,3 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-
